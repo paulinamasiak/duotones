e3d50537eabf1b674a11b6ef71e60443a0a4e58d
Paulina Masiak <paula.masiak@gmail.com>
2020-12-17 11:00:18 +0100

    Add theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, ThemeProvider } from '@emotion/core';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,45 @@
+const theme = {
+  breakpoints: {},
+  colors: {
+    common: {
+      black: '#000',
+      white: '#fff',
+    },
+    primary: {
+      light: '#cfc8ff',
+      main: '#6c5dd3',
+    },
+    secondary: {
+      main: '#3f8cff',
+    },
+    error: {
+      main: '#ff754c',
+    },
+    warning: {
+      main: '#ffce73',
+    },
+    info: {
+      main: '#a0d7e7',
+    },
+    sussess: {
+      main: '#7fba7a',
+    },
+    grey: {},
+    text: {
+      primary: '#fff',
+      secondary: '#808191',
+    },
+    divider: 'rgba(228, 228, 228, 0.1)',
+    background: {
+      default: '#242731',
+    },
+    action: {},
+  },
+  shadows: {},
+  typography: {},
+  shape: {},
+  transitions: {},
+  zIndex: {},
+};
+
+export default theme;
