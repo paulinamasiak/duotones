0c8072f44b296ed35316cef75f1eb949a1152a35
Paulina Masiak <paula.masiak@gmail.com>
2020-12-18 10:36:17 +0100

    Move colors definitions to separate file

---
+++ src/styles/colors.js
@@ -0,0 +1,43 @@
+const colors = {
+  common: {
+    black: '#000',
+    white: '#fff',
+  },
+  primary: {
+    light: '#cfc8ff',
+    main: '#6c5dd3',
+    // TODO: dark
+  },
+  secondary: {
+    main: '#3f8cff',
+    // TODO: light, dark
+  },
+  error: {
+    main: '#ff754c',
+    // TODO: light, dark
+  },
+  warning: {
+    main: '#ffce73',
+    // TODO: light, dark
+  },
+  info: {
+    main: '#a0d7e7',
+    // TODO: light, dark
+  },
+  sussess: {
+    main: '#7fba7a',
+    // TODO: light, dark
+  },
+  grey: {},
+  text: {
+    primary: '#fff',
+    secondary: '#808191',
+  },
+  divider: 'rgba(228, 228, 228, 0.1)',
+  background: {
+    default: '#242731',
+  },
+  action: {},
+};
+
+export default colors;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,45 +1,24 @@
+import colors from './colors';
 const theme = {
-  breakpoints: {},
-  colors: {
-    common: {
-      black: '#000',
-      white: '#fff',
-    },
-    primary: {
-      light: '#cfc8ff',
-      main: '#6c5dd3',
-    },
-    secondary: {
-      main: '#3f8cff',
-    },
-    error: {
-      main: '#ff754c',
-    },
-    warning: {
-      main: '#ffce73',
-    },
-    info: {
-      main: '#a0d7e7',
-    },
-    sussess: {
-      main: '#7fba7a',
-    },
-    grey: {},
-    text: {
-      primary: '#fff',
-      secondary: '#808191',
-    },
-    divider: 'rgba(228, 228, 228, 0.1)',
-    background: {
-      default: '#242731',
-    },
-    action: {},
+  breakpoints: {
+    // TODO
   },
-  shadows: {},
-  typography: {},
-  shape: {},
-  transitions: {},
-  zIndex: {},
+  colors,
+  shadows: {
+    // TODO
+  },
+  typography: {
+    // TODO
+  },
+  shape: {
+    // TODO
+  },
+  transitions: {
+    // TODO
+  },
+  zIndex: {
+    // TODO
+  },
 };
 
 export default theme;
