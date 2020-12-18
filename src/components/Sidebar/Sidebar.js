68219cf2ca38d1dc1432b8302ffc941161b20de3
Paulina Masiak <paula.masiak@gmail.com>
2020-12-18 10:55:18 +0100

    Basic styles for TopBar and Sidebar components

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import styled from '@emotion/styled/macro';
 
-const Sidebar = styled.div((props) => ({
-  background: props.theme.colors.background.default,
+const Sidebar = styled.div(({ theme }) => ({
+  background: theme.colors.background.default,
   position: 'fixed',
   top: '0px',
   left: '0px',
   width: '256px',
   height: '100vh',
   overflow: 'hidden',
+  boxShadow: `inset -1px 0 0 0 ${theme.colors.divider}`,
 }));
 
 export default Sidebar;
--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import styled from '@emotion/styled/macro';
 
-const TopBar = () => {
-  return <div>TopBar</div>;
-};
+const TopBar = styled.div(({ theme }) => ({
+  background: theme.colors.background.default,
+  position: 'fixed',
+  left: '256px',
+  right: '0px',
+  zIndex: '10',
+  height: '96px',
+  boxShadow: `inset 0 -1px 0 0 ${theme.colors.divider}`,
+}));
 
 export default TopBar;
