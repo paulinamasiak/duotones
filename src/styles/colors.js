fed17d5d82561cc54c5a93198b06f3d122541af0
Paulina Masiak <paula.masiak@gmail.com>
2021-01-27 10:47:32 +0100

    Update colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -19,33 +19,54 @@ const colors = {
     contrastText: '#fff',
   },
   error: {
-    main: '#ff754c',
-    // TODO: light, dark
+    light: '#e57373',
+    main: '#f44336',
+    dark: '#d32f2f',
+    contrastText: '#fff',
   },
   warning: {
+    light: '#ffffa3',
     main: '#ffce73',
-    // TODO: light, dark
+    dark: '#c99d44',
+    contrastText: '#000',
   },
   info: {
+    light: '#d3ffff',
     main: '#a0d7e7',
-    // TODO: light, dark
+    dark: '#6fa6b5',
+    contrastText: '#000',
   },
   sussess: {
+    light: '#b0eda9',
     main: '#7fba7a',
-    // TODO: light, dark
+    dark: '#508a4d',
+    contrastText: '#000',
   },
-  grey: {},
+  grey: {
+    50: '#e4e4e4',
+    100: '#cdcdcd',
+    200: '#b6b6b6',
+    300: '#a0a0a0',
+    400: '#898989',
+    500: '#727272',
+    600: '#5b5b5b',
+    700: '#444444',
+    800: '#2e2e2e',
+    900: '#171717',
+  },
   text: {
     primary: '#fff',
-    secondary: '#808191',
+    secondary: 'rgba(228, 228, 228, 0.7)',
+    disabled: 'rgba(228, 228, 228, 0.5)',
   },
   divider: 'rgba(228, 228, 228, 0.1)',
   background: {
-    card: '#242731',
-    default: '#1f2128',
+    card: '#2b2d35',
+    default: '#20222a',
   },
   action: {
     hoverDarken: 0.05,
+    hoverOpacity: 0.08,
   },
 };
 
