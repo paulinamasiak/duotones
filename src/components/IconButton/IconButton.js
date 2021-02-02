786682c32c60293d736e9037aae5092c9fe9b114
Paulina Masiak <paula.masiak@gmail.com>
2021-02-02 10:12:33 +0100

    IconButton component

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -1,23 +1,84 @@
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled/macro';
+import { shade, tint, rgba } from 'polished';
 import ButtonBase from 'components/ButtonBase';
 
+const COLOR = {
+  PRIMARY: 'primary',
+  SECONDARY: 'secondary',
+};
+
+const SIZE = {
+  SMALL: 'small',
+  MEDIUM: 'medium',
+  LARGE: 'large',
+};
+
 const baseStyles = ({ theme }) => ({
-  // TODO
+  borderRadius: theme.shape.borderRadius.sm,
+  ...theme.transitions.create('background-color', {
+    duration: theme.transitions.duration.short,
+  }),
 });
 
-const colorStyles = ({ theme, color = 'default' }) => ({
-  // TODO
+const colorStyles = ({ theme, color = COLOR.PRIMARY }) => ({
+  backgroundColor: theme.colors[color].main,
+  color: theme.colors[color].contrastText,
+
+  '&:hover': {
+    backgroundColor: tint(0.1, theme.colors[color].main),
+  },
+
+  '&:active': {
+    backgroundColor: shade(0.1, theme.colors[color].main),
+  },
 });
 
-const IconButton = styled(ButtonBase)(baseStyles, colorStyles);
+const sizeStyles = ({ theme, size = SIZE.MEDIUM }) => ({
+  ...(size === SIZE.SMALL && {
+    height: '40px',
+    width: '40px',
+  }),
+  ...(size === SIZE.MEDIUM && {
+    height: '48px',
+    width: '48px',
+  }),
+  ...(size === SIZE.LARGE && {
+    height: '56px',
+    width: '56px',
+  }),
+});
+
+const disabledStyles = ({ theme, color = COLOR.PRIMARY, disabled = false }) =>
+  disabled && {
+    cursor: 'default',
+    backgroundColor: rgba(
+      theme.colors.default.main,
+      theme.colors.action.disabledOpacity,
+    ),
+    color: rgba(
+      theme.colors[color].contrastText,
+      theme.colors.action.disabledOpacity,
+    ),
+  };
+
+const IconButton = styled(ButtonBase)(
+  baseStyles,
+  colorStyles,
+  sizeStyles,
+  disabledStyles,
+);
 
 IconButton.propTypes = {
-  color: PropTypes.oneOf(['default', 'primary', 'secondary']),
+  color: PropTypes.oneOf([COLOR.PRIMARY, COLOR.SECONDARY]),
+  size: PropTypes.oneOf([SIZE.SMALL, SIZE.MEDIUM, SIZE.LARGE]),
+  disabled: PropTypes.bool,
 };
 
 IconButton.defaultProps = {
-  color: 'default',
+  color: COLOR.PRIMARY,
+  size: SIZE.MEDIUM,
+  disabled: false,
 };
 
 export default IconButton;
--- src/components/IconButton/IconButtons.stories.js
+++ src/components/IconButton/IconButtons.stories.js
@@ -9,11 +9,6 @@ export default {
 
 const Template = (args) => <IconButton {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-  children: <SearchIcon />,
-};
-
 export const Primary = Template.bind({});
 Primary.args = {
   children: <SearchIcon />,
@@ -24,4 +19,28 @@ export const Secondary = Template.bind({});
 Secondary.args = {
   children: <SearchIcon />,
   color: 'secondary',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  children: <SearchIcon width={20} height={20} />,
+  size: 'small',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  children: <SearchIcon />,
+  size: 'medium',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  children: <SearchIcon />,
+  size: 'large',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: <SearchIcon />,
+  disabled: true,
 };
